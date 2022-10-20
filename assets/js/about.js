12031afec8c50d20c25959802ba725f359a2bdd8
ndaboom <ndabosam084@gmail.com>
2022-10-20 19:30:53 +0200

    About page popup menu not showing up fixed

---
+++ assets/js/about.js
@@ -0,0 +1,32 @@
+// HEADER POPUP SCRIPT
+
+const toggleMenu = document.querySelector('.toggleNav');
+const popup = document.querySelector('.popup');
+const close = document.querySelector('.close_icon');
+const menuOptions = document.querySelectorAll('.popup_menu_option');
+
+// Modal menu toggle
+toggleMenu.addEventListener('click', () => {
+  popup.classList.remove('hide');
+});
+
+// Close mobile menu
+close.addEventListener('click', () => {
+  popup.classList.add('hide');
+});
+
+// Add click listener to mobile menu
+menuOptions.forEach((option) => {
+  option.addEventListener('click', () => {
+    popup.classList.add('hide');
+  });
+});
+
+// Modal close event
+modalCloseBtn.addEventListener('click', () => {
+  modalSection.classList.add('hide');
+});
+
+modalSection.addEventListener('click', () => {
+  modalSection.classList.add('hide');
+});
